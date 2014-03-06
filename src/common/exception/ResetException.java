c666d9af3a9bccff2c4ab651b7fdda0a11c3dd46
dcvan <dcvan24@gmail.com>
2014-03-06 13:51:34 -0500

    1. make-up commit

---
+++ src/common/exception/ResetException.java
@@ -0,0 +1,13 @@
+package common.exception;
+
+public class ResetException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ResetException(String msg){
+		super(msg);
+	}
+}
